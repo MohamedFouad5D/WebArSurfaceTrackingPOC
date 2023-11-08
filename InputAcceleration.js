c29eb80e7a39f89f953cbe30440f778fc9932077
ahmedaglan1 <ahmedaglan993@gmail.com>
2023-11-08 10:53:26 +0200

    Acceleration

--- InputAcceleration.js
+++ InputAcceleration.js
@@ -6,7 +6,7 @@ function AddAccelerationEventX(objSource, msg)
         eventAccelerationX = function(event)
         {
             //console.log(event.acceleration.x + ' m/s2');
-            gameInstance.SendMessage(objSource, msg, event.alpha);
+            gameInstance.SendMessage(objSource, msg, event.acceleration.x);
         }
     }
     window.addEventListener('devicemotion', eventAccelerationX);
