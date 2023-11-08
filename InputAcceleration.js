75ca7066777a9e6d6632bcf2538a1c6bc891ed5b
ahmedaglan1 <ahmedaglan993@gmail.com>
2023-11-08 12:20:43 +0200

    Update InputAcceleration.js

--- InputAcceleration.js
+++ InputAcceleration.js
@@ -15,6 +15,6 @@ function RemoveInputAccelerationEventX()
 {
     if (eventAccelerationX != undefined)
     {
-        window.removeEventListener('deviceorientation', eventAccelerationX);
+        window.removeEventListener('devicemotion', eventAccelerationX);
     }
 }
