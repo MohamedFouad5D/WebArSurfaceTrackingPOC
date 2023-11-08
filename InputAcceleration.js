bdb60b26b4716ec51ef36511669e7b6358f5005c
ahmedaglan1 <ahmedaglan993@gmail.com>
2023-11-08 12:55:23 +0200

    Update InputAcceleration.js

--- InputAcceleration.js
+++ InputAcceleration.js
@@ -1,24 +1,22 @@
 var eventAccelerationX = null;
-function AddAccelerationEventX(objSource, msg)
-{
-    if(eventAccelerationX == undefined)
-    {
-        eventAccelerationX = function(event)
-        {
+
+function AddAccelerationEventX(objSource, msg) {
+    if (eventAccelerationX == undefined) {
+        eventAccelerationX = function (event) {
             var accelData = {
                 alpha: event.alpha,
                 beta: event.beta,
-                gamma: event.gamma      
+                gamma: event.gamma
             };
-            gameInstance.SendMessage(objSource, msg, accelData);
+            var data = JSON.stringify(accelData);
+            gameInstance.SendMessage(objSource, msg, data);
         }
     }
     window.addEventListener('deviceorientation', eventAccelerationX);
 }
-function RemoveInputAccelerationEventX()
-{
-    if (eventAccelerationX != undefined)
-    {
+
+function RemoveInputAccelerationEventX() {
+    if (eventAccelerationX != undefined) {
         window.removeEventListener('deviceorientation', eventAccelerationX);
     }
 }
