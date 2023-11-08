c3839144340a621df3d69b2acb7e3ad94be66ccc
ahmedaglan1 <ahmedaglan993@gmail.com>
2023-11-08 15:38:57 +0200

    new

--- InputAcceleration.js
+++ InputAcceleration.js
@@ -4,7 +4,7 @@ function AddAccelerationEventX(objSource, msg) {
     if (eventAccelerationX == undefined) {
         eventAccelerationX = function (event) {
             var accelData = {
-                alpha: event.alpha,
+                alpha: event..alpha,
                 beta: event.beta,
                 gamma: event.gamma
             };
