e7b776aabd0428ec279d8c89826f7d0f4987fa0f
ahmedaglan1 <ahmedaglan993@gmail.com>
2023-11-08 15:46:34 +0200

    new test

--- InputAcceleration.js
+++ InputAcceleration.js
@@ -4,10 +4,18 @@ function AddAccelerationEventX(objSource, msg) {
     if (eventAccelerationX == undefined) {
         eventAccelerationX = function (event) {
             var accelData = {
-                alpha: event..alpha,
+                alpha: event.alpha,
                 beta: event.beta,
                 gamma: event.gamma
             };
+            if (accelData.beta > 90) {
+                accelData.beta = 90;
+            }
+            if (accelData.beta < -90) {
+                accelData.beta = -90;
+            }
+            accelData.beta += 90;
+            accelData.gamma += 90;
             var data = JSON.stringify(accelData);
             gameInstance.SendMessage(objSource, msg, data);
         }
